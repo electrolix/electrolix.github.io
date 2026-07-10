4e17105833f262fb6338f6658ecb296a31bc8e14
Nitish Patil <nitishpatil.in@gmail.com>
2026-07-10 17:57:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ fetch("header.html")
         document.getElementById("header").innerHTML = data;
 
         // Mobile Menu
-        const menuBtn = document.querySelector(".menu-btn");
-        const nav = document.querySelector("nav");
+       const menuBtn=document.querySelector(".menu-btn");
+const nav=document.querySelector("nav");
 
-        if(menuBtn && nav){
-            menuBtn.onclick = () => {
-                nav.classList.toggle("active");
+menuBtn.onclick=()=>{
 
-                if(nav.classList.contains("active")){
-                    menuBtn.innerHTML = '<i class="fas fa-times"></i>';
-                }else{
-                    menuBtn.innerHTML = '<i class="fas fa-bars"></i>';
-                }
-            };
-        }
-    });
+nav.classList.toggle("active");
 
+if(nav.classList.contains("active")){
+menuBtn.innerHTML='<i class="fas fa-times"></i>';
+}
+else{
+menuBtn.innerHTML='<i class="fas fa-bars"></i>';
+}
+
+}
+        
 // Load Footer
 fetch("footer.html")
     .then(response => response.text())
