5d275f5c55283c197353d2787152e454749a5977
Nitish Patil <nitishpatil.in@gmail.com>
2026-07-10 18:03:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+
+menuBtn.onclick = () => {
+
+    nav.classList.toggle("active");
+
+    if(nav.classList.contains("active")){
+        menuBtn.innerHTML='<i class="fas fa-times"></i>';
+    }
+    else{
+        menuBtn.innerHTML='<i class="fas fa-bars"></i>';
+    }
+
+}
