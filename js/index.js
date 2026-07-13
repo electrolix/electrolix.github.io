8a3359700dbf056d9149b8a6478e11724d353d9a
Nitish Patil <nitishpatil.in@gmail.com>
2026-07-13 18:37:06 +0530

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById("menuBtn");
+
+const nav = document.getElementById("nav");
+
+menuBtn.addEventListener("click",()=>{
+
+    nav.classList.toggle("active");
+
+});
