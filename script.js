c05f0bcd8825885f4e4e681efb3c2bbac7862a4e
Nitish Patil <nitishpatil.in@gmail.com>
2026-07-13 11:31:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,25 @@ fetch(file)
 
 document.getElementById(id).innerHTML = data;
 
+
+// Header loaded, now activate menu
+
+if(id === "header"){
+
+const menuBtn = document.getElementById("menuBtn");
+
+const nav = document.getElementById("nav");
+
+
+menuBtn.addEventListener("click",()=>{
+
+    nav.classList.toggle("active");
+
+});
+
+}
+
+
 });
 
 }
