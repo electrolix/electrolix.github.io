eeb15187c71b3d401f188f01dfe4bb3de1413516
Nitish Patil <nitishpatil.in@gmail.com>
2026-08-21 16:07:45 +0530

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -27,11 +27,11 @@ const blogLinks = [
 
     "https://electrolix.github.io/finolex-wires-for-lighting-guide.html",
 
-    "https://electrolix.github.io/finolex-0-75-mm-wire-complete-guide.html"
+    "https://electrolix.github.io/finolex-0-75-mm-wire-complete-guide.html",
 
-    "https://electrolix.github.io/understanding-finolex-wires-power-sockets.html"
+    "https://electrolix.github.io/understanding-finolex-wires-power-sockets.html",
 
-    "https://electrolix.github.io/finolex-industrial-cables-types-sizes-guide.html"     
+    "https://electrolix.github.io/finolex-industrial-cables-types-sizes-guide.html",     
 
 ];
 
